51a951b442275e5598d506c6cf36987e763fab43
ChrisRackauckas <me@chrisrackauckas.com>
2016-10-26 18:37:08 -0700

    linear complete

--- test/linear.jl
+++ test/linear.jl
@@ -9,10 +9,22 @@ y_rhs = "-k*x/m"
 vardict = Dict("x"=>x_rhs,"y"=>y_rhs)
 py_vardict = PyDict(vardict)
 
+# keys()...
+
 DSargs = ds.args()
 DSargs[:name] = "SHM"
 DSargs[:ics] = py_icdict
 DSargs[:pars] = py_pardict
 DSargs[:tdata] = [0,20]
 DSargs[:varspecs] = py_vardict
-DS = ds.Generator.Euler_ODEsystem(DSargs)
+DS = ds.Generator[:Vode_ODEsystem](DSargs)
+traj = DS[:compute]("demo")
+pts = traj[:sample]()
+pts_dict = pts[:__dict__]
+d = Dict{Symbol,Vector{Float64}}()
+d[Symbol(pts_dict["indepvarname"])] = pts_dict["indepvararray"]
+names = pts_dict["_ix_name_map"]
+arrs = pts_dict["coordarray"]
+for i in 1:length(names)
+  d[Symbol(names[i])] = arrs[i,:]
+end
