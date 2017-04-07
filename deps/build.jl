f4d0e83550668fc87128f800fe64ee9eb4f674c2
ChrisRackauckas <chrisrackauckas@gmail.com>
2017-04-06 21:02:46 -0700

    formatting

--- deps/build.jl
+++ deps/build.jl
@@ -10,21 +10,20 @@ if PyCall.conda
   else
     pip = joinpath(Conda.BINDIR, "pip")
     run(`$pip install pydstool`)
-  end 
+  end
 else
 	try
 		pyimport("PyDSTool")
 	catch ee
 		typeof(ee) <: PyCall.PyError || rethrow(ee)
 		warn("""
-Python Dependancies not installed
-Please either:
- - Rebuild PyCall to use Conda, by running in the julia REPL:
-    - `ENV["PYTHON"]=""; Pkg.build("PyCall"); Pkg.build("PyDSTool")`
- - Or install the depencences, eg by running pip
-	- `pip install pydstool`
-	"""
-		)
+				Python Dependancies not installed
+				Please either:
+				 - Rebuild PyCall to use Conda, by running in the julia REPL:
+				    - `ENV["PYTHON"]=""; Pkg.build("PyCall"); Pkg.build("PyDSTool")`
+				 - Or install the depencences, eg by running pip
+					- `pip install pydstool`
+				 """)
 	end
 
 end
